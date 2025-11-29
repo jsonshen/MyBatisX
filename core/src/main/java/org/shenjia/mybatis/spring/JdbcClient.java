3398853cc63acb8c5a7b592014f42523dd3674c8
jsonshen <json.shen@foxmail.com>
2025-11-29 17:50:34 +0800

    v2.2.0

--- core/src/main/java/org/shenjia/mybatis/spring/JdbcClient.java
+++ core/src/main/java/org/shenjia/mybatis/spring/JdbcClient.java
@@ -3,12 +3,19 @@ package org.shenjia.mybatis.spring;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.logging.LogFactory;
+import org.mybatis.dynamic.sql.select.SelectModel;
+import org.mybatis.dynamic.sql.util.Buildable;
 import org.mybatis.dynamic.sql.util.spring.NamedParameterJdbcTemplateExtensions;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
@@ -36,5 +43,16 @@ public class JdbcClient extends NamedParameterJdbcTemplateExtensions {
 			DataSourceUtils.releaseConnection(con, ds);
 		}
 	}
+	
+	public Optional<Map<String, Object>> selectForMap(Buildable<SelectModel> selectStatement) {
+        return selectOne(selectStatement, new ColumnMapRowMapper());
+    }
 
+	public <T> Optional<T> selectForEntity(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
+        return selectOne(selectStatement, new BeanPropertyRowMapper<T>(mappedClass));
+    }
+	
+	public <T> Optional<T> selectForObject(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
+        return selectOne(selectStatement, new SingleColumnRowMapper<T>(mappedClass));
+    }
 }
