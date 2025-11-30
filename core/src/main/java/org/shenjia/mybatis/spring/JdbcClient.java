63b393b0e2363a9f1911a5ee48293c38dc1178ea
jsonshen <json.shen@foxmail.com>
2025-11-30 10:34:24 +0800

    v2.2.1

--- core/src/main/java/org/shenjia/mybatis/spring/JdbcClient.java
+++ core/src/main/java/org/shenjia/mybatis/spring/JdbcClient.java
@@ -3,8 +3,11 @@ package org.shenjia.mybatis.spring;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 
 import javax.sql.DataSource;
 
@@ -15,11 +18,15 @@ import org.mybatis.dynamic.sql.util.spring.NamedParameterJdbcTemplateExtensions;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class JdbcClient extends NamedParameterJdbcTemplateExtensions {
+	
+	private static final Map<Class<?>, RowMapper<?>> ROW_MAPPERS = new ConcurrentHashMap<>();
+	private static final ColumnMapRowMapper MAP_ROW_MAPPER = new ColumnMapRowMapper();
 
 	private DatabaseMetaData metadata;
 
@@ -45,14 +52,36 @@ public class JdbcClient extends NamedParameterJdbcTemplateExtensions {
 	}
 	
 	public Optional<Map<String, Object>> selectForMap(Buildable<SelectModel> selectStatement) {
-        return selectOne(selectStatement, new ColumnMapRowMapper());
+        return selectOne(selectStatement, MAP_ROW_MAPPER);
     }
 
 	public <T> Optional<T> selectForEntity(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
-        return selectOne(selectStatement, new BeanPropertyRowMapper<T>(mappedClass));
+        return selectOne(selectStatement, getRowMapper(mappedClass, () -> new BeanPropertyRowMapper<T>(mappedClass)));
     }
 	
 	public <T> Optional<T> selectForObject(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
-        return selectOne(selectStatement, new SingleColumnRowMapper<T>(mappedClass));
+        return selectOne(selectStatement, getRowMapper(mappedClass, () -> new SingleColumnRowMapper<T>(mappedClass)));
     }
+	
+	public List<Map<String, Object>> selectForMaps(Buildable<SelectModel> selectStatement) {
+        return selectList(selectStatement, MAP_ROW_MAPPER);
+    }
+
+	public <T> List<T> selectForEntities(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
+        return selectList(selectStatement, getRowMapper(mappedClass, () -> new BeanPropertyRowMapper<T>(mappedClass)));
+    }
+	
+	public <T> List<T> selectForObjects(Buildable<SelectModel> selectStatement, Class<T> mappedClass) {
+        return selectList(selectStatement, getRowMapper(mappedClass, () -> new SingleColumnRowMapper<T>(mappedClass)));
+    }
+	
+	@SuppressWarnings("unchecked")
+	private static <T> RowMapper<T> getRowMapper(Class<T> mappedClass, Supplier<RowMapper<T>> supplier) {
+		RowMapper<?> rowMapper = ROW_MAPPERS.get(mappedClass);
+		if (null == rowMapper) {
+			rowMapper = supplier.get();
+			ROW_MAPPERS.put(mappedClass, rowMapper);
+		}
+		return (RowMapper<T>) rowMapper;
+	}
 }
